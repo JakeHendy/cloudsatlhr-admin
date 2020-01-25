7c2764db87cfb284994278e52c9a4bdf7ca5f687
Jake <me@jakehendy.com>
2020-01-25 22:29:17 +0000

    Published first website

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="createNewTodo">Add Todo</button>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{todo.name}} - {{todo.description}}
+      </li>
+    </ul>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import API, {  graphqlOperation } from '@aws-amplify/api';
+// eslint-disable-next-line
+import { createTodo } from "./graphql/mutations";
+
+// other imports
+import { onCreateTodo } from './graphql/subscriptions'
+
+// other imports
+import { listTodos } from './graphql/queries'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      todos: []
+    }
+  },
+  methods :{
+    async createNewTodo(){ 
+      const todo = { name: "Use AppSync" , description: "Realtime and Offline"}
+      await API.graphql(graphqlOperation(createTodo, { input: todo }))
+    }, 
+    async getData(){
+      const todoData = await API.graphql(graphqlOperation(listTodos))
+      this.todos.push(...this.todos, ...todoData.data.listTodos.items);
+    },
+    subscribe(){
+      API.graphql(graphqlOperation(onCreateTodo)).subscribe({
+        next: (eventData) => {
+          const todo = eventData.value.data.onCreateTodo;
+          this.todos.push(todo);
+        }
+      })
+    }
+  },
+  created(){
+    this.getData()
+    this.subscribe()
   }
-}
+};
+
 </script>
 
 <style>
@@ -21,7 +59,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
